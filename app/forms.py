9b88b80acf516fa8056d12e15626c34f2a91ee5d
Clive <clive.gross1@gmail.com>
2016-11-26 02:03:51 +0000

    added material design lite, sqlite connection

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email
 
-class LoginForm(Form):
-    email = StringField('email', validators=[Email()])
-    remember_me = BooleanField('remember_me', default=False)
 
+class LoginForm(Form):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -10,7 +10,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', 
-                            title='Sign In',
-                            form=form)
-
+    if form.validate_on_submit():
+        flash('Login requested for email="%s", remember_me=%s' %
+              (form.email.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template(
+        'login.html',
+        form=form
+    )
